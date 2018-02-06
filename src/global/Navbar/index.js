e63766fd3d5dee76a39e74b75a9da622b5812aba
Gabriel Martin <gabriel.martin91@outlook.com>
2018-02-06 14:33:29 -0800

    basic folder structure.

---
+++ src/global/Navbar/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <div>
+        <p>Navbar</p>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
